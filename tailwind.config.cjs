41ceeb9a55ea9cc0333168250ca33e9c59aeffd3
cris <ctovar@abexuspr.com>
2022-10-16 11:44:59 -0500

    🎶 set backgroundImage for the card

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -43,6 +43,13 @@ module.exports = {
       height: {
         80: '80px',
       },
+      minHeight: {
+        590: '590px',
+      },
+      backgroundImage: {
+        'hero-pattern':
+          "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+      },
     },
   },
   plugins: [],
