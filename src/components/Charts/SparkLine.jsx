8031110a7bd568cee5d50dde88a11fb3f6a30bb1
cris <ctovar@abexuspr.com>
2022-10-16 14:24:44 -0500

    🇪🇸 SparkLine v1

--- src/components/Charts/SparkLine.jsx
+++ src/components/Charts/SparkLine.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from 'react';
+import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
+import { useStateContext } from '../../context/ContexProvider';
+/* import { useStateContext } from '../../context/ContextProvider'; */
 
-const SparkLine = () => {
+const SparkLine = ({ id, data, type, color, width, height }) => {
+  console.log(`🇸🇱%cSparkLine.jsx:7 - data`, 'font-weight:bold; background:#25da00;color:#fff;'); //DELETEME
+  console.log(data); // DELETEME
+  const { currentColor } = useStateContext();
+
   return (
-    <div>SparkLine</div>
-  )
-}
+    <SparklineComponent
+      id={id}
+      height={height}
+      width={width}
+      lineWidth={1}
+      valueType="Numeric"
+      fill={color}
+      border={{ color: currentColor, width: 2 }}
+      dataSource={data}
+      xName="x"
+      yName="yval"
+      type={type}
+      tooltipSettings={{
+        visible: true,
+        format: '${x} : data ${yval}',
+        trackLineSettings: {
+          visible: true,
+          fill: currentColor,
+        },
+      }}
+      markerSettings={{ visible: ['All'], size: 2.5, fill: currentColor }}
+    >
+      <Inject services={[SparklineTooltip]} />
 
-export default SparkLine
+    </SparklineComponent>
+  );
+};
+
+export default SparkLine;
